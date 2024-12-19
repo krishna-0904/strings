fa42fa2277f3cd5a49c357cc853cb6c57ad9e7cc
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-19 08:49:40 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,10 @@
+#1000=-7-6-5-4-3-2-1
+sales="krishna"
+#1000=0123456
+#start and step
+print(sales[0:5])
+print(sales[-6:-2])
+#star,stop,step
+print(sales[1:7:2])
+print(sales[::1])
+print(sales[::-1])
